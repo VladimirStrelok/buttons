094a2a0ff885a1613261f92df6eac82ec4fa4bfe
Alexander Glica <alexander.glica@gmail.com>
2014-04-08 22:46:03 -0400

    added document ready to js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-
-var disabled=false;
-$("#disable").click(function(){
+$(function() {
+ var disabled=false;
+ $("#disable").click(function(){
   if(!disabled)
   {
-    $("#fieldset").attr("disabled", "disabled");
-    $("#disable").text('Enable')
-    disabled=true;
-    $("#disable").switchClass( 'btn-danger', 'btn-alert')
+   $("#fieldset").attr("disabled", "disabled");
+   $("#disable").text('Enable')
+   disabled=true;
+   $("#disable").switchClass( 'btn-danger', 'btn-alert')
   }
   else
   {
-    $("#fieldset").removeAttr("disabled");
-    $("#disable").text('Disable')
-    disabled=false;
-    $("#disable").switchClass( 'btn-alert', 'btn-danger')
+   $("#fieldset").removeAttr("disabled");
+   $("#disable").text('Disable')
+   disabled=false;
+   $("#disable").switchClass( 'btn-alert', 'btn-danger')
   }
+ });
 });
